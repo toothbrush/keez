079980ff0292ff4b9efc7dc14d35a063d56f8b77
Paul <423357+toothbrush@users.noreply.github.com>
2020-09-18 16:38:37 +1000

    Add some documentation to the various options.

--- src/main.rs
+++ src/main.rs
@@ -8,6 +8,7 @@ use tokio::runtime::Runtime;
 struct Keez {
     // Define our global flags here.
     #[structopt(short = "n", long)]
+    /// Avoid any write operations on the Parameter Store.
     dry_run: bool,
     #[structopt(subcommand)]
     cmd: KeezCommand,
@@ -15,25 +16,40 @@ struct Keez {
 
 #[derive(Debug, StructOpt)]
 enum KeezCommand {
+    /// Transplant all parameters under a given prefix to another prefix.
     Copy {
+        /// The path prefix for selecting parameters to copy.
         source: String,
+        /// The path where you would like to copy parameters to.
         destination: String,
     },
+    /// Interactively create a new set of parameters.
     Create {},
+    /// Interactively edit existing parameters under a given prefix.
     Edit {
+        /// The path prefix for selecting parameters to edit.
         prefix: String,
     },
+    /// Export is useful for migrating a group of parameters to another AWS account or region.
     Export {
+        /// The path prefix for selecting parameters to export.
         source: String,
         #[structopt(long, parse(from_os_str))]
+        /// File to export parameters to, prior to importing to another account.
         export_filename: PathBuf,
         #[structopt(short = "I", long)]
+        /// For debugging: print results of export to stdout.
         insecure_output: bool,
     },
+    /// Import is to be used with the output from the keez-export command.
     Import {
+        /// The target path for importing parameters.
+        destination: String,
         #[structopt(long, parse(from_os_str))]
+        /// File to import parameters from, obtained by exporting with keez-export.
         import_filename: PathBuf,
         #[structopt(short, long)]
+        /// Whether to interactively edit values prior to importing.
         edit: bool,
     },
 }
