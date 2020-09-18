24e85c4ada0e4648656bad0d3d0757e70bbd84f6
Paul <423357+toothbrush@users.noreply.github.com>
2020-09-18 14:14:51 +1000

    First stab at pulling parameters from AWS.

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,51 @@
+use serde::Deserialize;
+use std::path::PathBuf;
+use structopt::StructOpt;
+use tokio::runtime::Runtime;
+
+#[derive(Debug, StructOpt)]
+#[structopt(about = "the well-known content tracker")]
+enum Git {
+    Add {
+        #[structopt(short)]
+        interactive: bool,
+        #[structopt(short)]
+        patch: bool,
+        #[structopt(parse(from_os_str))]
+        files: Vec<PathBuf>,
+    },
+    Fetch {
+        #[structopt(long)]
+        dry_run: bool,
+        #[structopt(long)]
+        all: bool,
+        repository: Option<String>,
+    },
+    Commit {
+        #[structopt(short)]
+        message: Option<String>,
+        #[structopt(short)]
+        all: bool,
+    },
+}
+
+#[derive(Deserialize, Debug)]
+struct Config {
+    foo: String,
+    bar: Vec<String>,
+    zar: u32,
+}
+
 fn main() {
-    println!("Hello, world!");
+    let matches = Git::from_args();
+    println!("{:?}", matches);
+    match matches {
+        Git::Add { .. } => {}
+        Git::Fetch { .. } => {}
+        Git::Commit { .. } => {}
+    }
+
+    let mut rt = Runtime::new().expect("failed to initialize runtime");
+    let conf = envy_store::from_path::<Config, _>("/demo");
+    println!("config {:#?}", rt.block_on(conf))
 }
