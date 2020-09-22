a559c55ece9bdb1813fe894e51e350bae2c6f5c3
Paul <423357+toothbrush@users.noreply.github.com>
2020-09-22 13:56:15 +1000

    Move Parameter Store logic into aws module.

--- src/aws/parameter_store.rs
+++ src/aws/parameter_store.rs
@@ -1,3 +1,8 @@
-pub fn get_parameters_by_path() {
-    println!("asdf");
+use std::collections::HashMap;
+use tokio::runtime::Runtime;
+
+pub fn get_parameters_by_path(path_prefix: String) {
+    let mut rt = Runtime::new().expect("failed to initialize runtime");
+    let conf = envy_store::from_path::<HashMap<String, String>, _>(path_prefix);
+    println!("config {:#?}", rt.block_on(conf));
 }
--- src/cli/cmd_export.rs
+++ src/cli/cmd_export.rs
@@ -1,17 +1,11 @@
-use std::collections::HashMap;
-use tokio::runtime::Runtime;
-
 use crate::aws;
 use crate::cli;
 
-pub fn run(_k: cli::Keez) {
-    println!("export command!");
-
-    let mut rt = Runtime::new().expect("failed to initialize runtime");
-    let conf = envy_store::from_path::<HashMap<String, String>, _>("/demo");
-    println!("config {:#?}", rt.block_on(conf));
-
-    aws::parameter_store::get_parameters_by_path();
-
-    return;
+pub fn run(
+    _args: cli::Keez,
+    _export_filename: std::path::PathBuf,
+    _insecure_output: bool,
+    source: String,
+) {
+    aws::parameter_store::get_parameters_by_path(source);
 }
