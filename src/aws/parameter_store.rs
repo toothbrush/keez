d73659fec4485d6ab481d347c9b10f589b3fa4f1
Paul <423357+toothbrush@users.noreply.github.com>
2020-09-23 14:22:48 +1000

    Paging over Parameter Store results works! 👌

--- src/aws/parameter_store.rs
+++ src/aws/parameter_store.rs
@@ -26,15 +26,14 @@ impl Parameters {
 }
 
 pub fn get_parameters_by_path(path_prefix: String) -> Parameters {
-    let ps = raw_parameters_by_path(path_prefix.clone());
-    return Parameters::new(path_prefix.clone());
+    let raw_parameters = raw_parameters_by_path(path_prefix.clone());
+    let result = Parameters::new(path_prefix.clone());
+    return result;
 }
 
 fn raw_parameters_by_path(
     path_prefix: String,
 ) -> Result<Vec<rusoto_ssm::Parameter>, Box<dyn error::Error>> {
-    println!("in raw_parameters_by_path...");
-
     let mut rt = runtime::Builder::new()
         .threaded_scheduler()
         .enable_all()
@@ -42,15 +41,35 @@ fn raw_parameters_by_path(
         .unwrap();
 
     let client = SsmClient::new(Default::default());
-    let res = rt.block_on(client.get_parameters_by_path(GetParametersByPathRequest {
-        path: path_prefix.clone(),
+
+    // Super awful, let's chuck out all the lovingly-crafted futures
+    // work and just run the request in a blocking fashion on the main
+    // thread.
+    let mut req = GetParametersByPathRequest {
+        path: path_prefix.to_owned(),
         with_decryption: Some(true),
         recursive: Some(true),
         ..GetParametersByPathRequest::default()
-    }));
+    };
 
-    println!("{:?}", res);
-    let mut vec = Vec::new();
+    let mut res = rt.block_on(client.get_parameters_by_path(req.clone()))?;
 
-    return Ok(vec);
+    let mut params: Vec<rusoto_ssm::Parameter> = Vec::new();
+    if let Some(new_params) = res.parameters {
+        params.extend(new_params.into_iter());
+    }
+
+    // Get next set of parameters if there's a next_token.
+    while let Some(next_token) = res.next_token {
+        req.next_token = Some(next_token);
+        res = rt.block_on(client.get_parameters_by_path(req.clone()))?;
+
+        if let Some(new_params) = res.parameters {
+            params.extend(new_params.into_iter());
+        }
+    }
+
+    println!("{:?}", params);
+
+    return Ok(params);
 }
