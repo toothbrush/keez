57e9776cffa38093309b92033a8d9e86b84e8582
Paul <423357+toothbrush@users.noreply.github.com>
2020-09-24 14:28:13 +1000

    cmd_import: Make a start reading & decrypting files.

--- src/cli/cmd_import.rs
+++ src/cli/cmd_import.rs
@@ -1,5 +1,6 @@
 use std::env;
 use std::fs;
+use std::fs::File;
 use std::io::prelude::*;
 use std::io::BufWriter;
 use std::path::Path;
@@ -9,5 +10,24 @@ use crate::cli;
 use crate::secrets;
 
 pub fn run(args: cli::Keez, import_filename: std::path::PathBuf, destination: String, _edit: bool) {
-    println!("Import secrets!");
+    // Create a path to the desired file
+    let path = Path::new(&import_filename);
+    let absolute_path = if path.is_absolute() {
+        path.to_path_buf()
+    } else {
+        env::current_dir().unwrap().join(path)
+    };
+
+    println!(
+        "Reading exported parameters from {}... ",
+        absolute_path.display()
+    );
+
+    // Open file read-only.
+    // Read the file contents for decrypting.
+    let encrypted_blob: Vec<u8> = fs::read(absolute_path).unwrap();
+    println!("{:?}", encrypted_blob);
+
+    let raw_yaml = secrets::symmetric_store::decrypt(encrypted_blob).unwrap();
+    println!("{}", raw_yaml);
 }
