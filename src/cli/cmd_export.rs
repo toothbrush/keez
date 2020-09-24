9b3b6bba78a6f552034a48642b7af0b3037e41ad
Paul <423357+toothbrush@users.noreply.github.com>
2020-09-24 10:52:34 +1000

    cmd_export: Call serde_yaml and give the user their parameters!

--- src/cli/cmd_export.rs
+++ src/cli/cmd_export.rs
@@ -4,7 +4,7 @@ use crate::cli;
 pub fn run(
     args: cli::Keez,
     _export_filename: std::path::PathBuf,
-    _insecure_output: bool,
+    insecure_output: bool,
     source: String,
 ) {
     let ps = aws::parameter_store::get_parameters_by_path(source);
@@ -14,8 +14,16 @@ pub fn run(
         println!("{:?}", ps);
     }
 
+    let unwrapped_parameterblob = ps.unwrap();
+
     println!(
         "Returned {:?} parameters from store.",
-        ps.unwrap().get_params().len()
+        unwrapped_parameterblob.get_params().len()
     );
+
+    let s = serde_yaml::to_string(&unwrapped_parameterblob).unwrap();
+
+    if insecure_output {
+        println!("{}", s);
+    }
 }
