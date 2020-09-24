a93cbae7e70d4faab717f6c550351a61f64c828d
Paul <423357+toothbrush@users.noreply.github.com>
2020-09-24 10:34:48 +1000

    Use debug flag in cmd_export.

--- src/cli/cmd_export.rs
+++ src/cli/cmd_export.rs
@@ -2,15 +2,14 @@ use crate::aws;
 use crate::cli;
 
 pub fn run(
-    _args: cli::Keez,
+    args: cli::Keez,
     _export_filename: std::path::PathBuf,
     _insecure_output: bool,
     source: String,
 ) {
     let ps = aws::parameter_store::get_parameters_by_path(source);
 
-    let debug = true; //TODO proper settings
-    if debug {
+    if args.debug {
         println!("Raw output from Parameter Store:");
         println!("{:?}", ps);
     }
