4279c11e2844ebac8348a6791116fea436777109
Paul <423357+toothbrush@users.noreply.github.com>
2020-09-25 14:40:05 +1000

    cmd_edit: Use new edit_loop and push_updated_parameters.

--- src/cli/cmd_edit.rs
+++ src/cli/cmd_edit.rs
@@ -1,9 +1,6 @@
-use std::fs;
-
-use mktemp::Temp;
-
 use crate::aws;
 use crate::cli;
+use crate::editor;
 
 pub fn run(args: cli::Keez, prefix: String) {
     let ps = aws::parameter_store::get_parameters_by_path(prefix);
@@ -22,11 +19,35 @@ pub fn run(args: cli::Keez, prefix: String) {
 
     let yaml_blob = serde_yaml::to_string(&unwrapped_parameterblob).unwrap();
 
-    // Create a temporary file somewhere.  When the variable goes out
-    // of scope, the mktemp crate takes care of cleaning it up.
-    let temp_file = Temp::new_file().unwrap();
-    // Write YAML blob to temp file, then edit.
-    fs::write(&temp_file, &yaml_blob).unwrap();
+    let new_yaml_blob = editor::edit_loop::interactive_edit(yaml_blob).unwrap();
 
-    println!("Opening {} for editing...", temp_file.display());
+    if args.debug {
+        println!("New YAML blob:");
+        println!("{}", new_yaml_blob);
+    }
+    // TODO re-open editor if something about the new YAML makes it
+    // unparsable, or if something goes wrong pushing to AWS API.
+
+    // Deserialize it back to a Rust type.
+    let deserialized: aws::parameter_store::ParameterCollection =
+        serde_yaml::from_str(&new_yaml_blob).unwrap();
+
+    if args.debug {
+        println!("Data structure after deserialization:");
+        println!("{:?}", deserialized);
+    }
+
+    println!("Edited blob contains the following keys:");
+    for (key, _param) in deserialized.get_parameters() {
+        println!("  - {}", key);
+    }
+
+    let write_mode = !args.dry_run; // TODO proper enum OperationMode with READ_ONLY vs READ_WRITE
+
+    aws::parameter_store::push_updated_parameters(
+        unwrapped_parameterblob,
+        deserialized,
+        write_mode,
+    )
+    .unwrap();
 }
